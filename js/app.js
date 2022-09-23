5a857e2ff991660c2d0c1a2276462bf8b9c6aab7
Maylon-tech <minatokiroisen77@gmail.com>
2022-09-23 17:16:10 +0900

    trending Page

--- js/app.js
+++ js/app.js
@@ -5,4 +5,43 @@ let navbar = document.querySelector('.menu')
 menu.addEventListener('click', () => {
     menu.classList.toggle('move')
     navbar.classList.toggle('active')  
+    bell.classList.remove('active')
 })
+
+// Notifications
+let bell = document.querySelector('.notification')
+const bellIcon = document.getElementById('bell-icon')
+
+bellIcon.addEventListener("click", () => {
+    bell.classList.toggle('active')
+})
+
+
+// Swiper code
+
+var swiper = new Swiper(".trending-content", {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        autoplay:{
+            delay:5000,
+            disableOnInteraction: false
+        },
+        breakpoints: {
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+          },
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+          },
+        },
+      });
